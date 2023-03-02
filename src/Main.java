42d4b2c311673c680ba947b5d2b9bbb49a8b9a5d
Георгий <voproshalo@yandex.ru>
2023-03-02 20:49:04 +0300

    Калькулятор умножения

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         int a = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число:");
         int b = new Scanner(System.in).nextInt();
-        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " * " + b + " = " + (a * b));
     }
 }
